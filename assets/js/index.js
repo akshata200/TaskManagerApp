572597d18342f480f1f859002bf081baf741ffb6
Akshata <akshata2202.01@gmail.com>
2021-07-01 09:50:17 +0530

    changed path for delete request

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,7 @@ const del = document.getElementsByClassName('delete');
 Array.from(del).forEach(delBtn => {
     delBtn.addEventListener('click', () => {
         const delId = delBtn.getAttribute("data-id");
-        let urldel = process.env.FETCH_TASKS_BY_EMAIL + `/api/users/${delId}`;
-        console.log(delId);
+        let urldel = `https://usertask-manger.herokuapp.com/api/users/${delId}`;
         fetch(urldel, {
             method: "DELETE",
         }).then(() => {
